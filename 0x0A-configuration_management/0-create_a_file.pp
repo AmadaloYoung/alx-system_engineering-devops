9cd00754dd6f4b266ca5c0a6fd4348f879778489
Giggs Bradly <giggsb9@gmail.com>
2022-10-19 15:09:20 +0300

    Create a File

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,8 @@
-# create a file in /tmp
-File {'/tmp/school':
-mode    => '0744',
-owner   => 'www-data',
-group   =>  'www-data',
-content => 'I love Puppet',
+# creates a file in /tmp
+
+file { '/tmp/school':
+  content =>'I love Puppet',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
 }
